3876c011e488a404a9f29411a49a76c988517f02
chloesli <40907810+chloesli@users.noreply.github.com>
2020-07-19 04:21:28 +1000

    changed login

--- Frontend/src/pages/UserSignUp.js
+++ Frontend/src/pages/UserSignUp.js
@@ -52,19 +52,19 @@ export class UserSignUp extends Component {
             <section className="section-wrap" id="signup">
             <h1>Be COVID Safer</h1>    
                 <label>
-                    First Name:
+                    First Name:<p></p>
                     <input type="text" name="fname" value={this.state.fname} onChange={this.handleChange}/>
                 </label>
                 <label>
-                    Last Name:
+                    Last Name:<p></p>
                     <input type="text" name="lname" value={this.state.lname} onChange={this.handleChange}/>
                 </label>
                 <label>
-                    Email Address:
+                    Email Address:<p></p>
                     <input type="email" name="email" value={this.state.email} onChange={this.handleChange}/>
                 </label>
                 <label>
-                    Password:
+                    Password:<p></p>
                     <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
                 </label>
                 <input type="submit" value="Submit" onClick={this.handleSubmit}/>
