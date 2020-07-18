351f0c3690ca207f0c5386f22181e29db6466383
chloesli <40907810+chloesli@users.noreply.github.com>
2020-07-19 09:41:59 +1000

    Update CustomerCheckIns.js

--- Frontend/src/components/CustomerCheckIns.js
+++ Frontend/src/components/CustomerCheckIns.js
@@ -10,17 +10,18 @@ export default class CustomerCheckIns extends Component {
             currentPlace: {},
             loading: true,
             place: [],
+            visitors:[],
         }
     }
     static contextType = AppContext;
     componentDidMount() {
-        const getBusiness= this.context.getBusinessPlace();
-        getBusiness.then((res) => {
+        const {getBusinessPlace, getAllVisitors}= this.context;
+        getBusinessPlace().then((res) => {
             console.log(res.data.place);
             this.setState({
                 currentPlace: res.data.place
             })
-            return true;
+            return res.data.place.id;
         }, (res) => {
             console.log("rejected");
             const message = res.response.data.message;
@@ -29,10 +30,16 @@ export default class CustomerCheckIns extends Component {
                     loading: false,
                 })
             }
-            return false;
+            return null;
         }).then((res) => {
             if(res) {
-                
+                getAllVisitors(res)
+                    .then((res) => {
+                        console.dir(res.data);
+                        this.setState({
+                            visitors: res.data
+                        })
+                    })
             }
         })
     }
